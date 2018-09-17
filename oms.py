7e955ece0ecc56b2dc1b99125dca956bbd1c2a7d
stevens-morons <42627085+stevens-morons@users.noreply.github.com>
2018-09-17 14:09:07 -0400

    Update oms.py

--- oms.py
+++ oms.py
@@ -2,11 +2,13 @@ import ccxt
 import datetime
 import time
 import math
-# import pandas as pd
+import pandas as pd
 import numpy as np
+from technical_indicators import ATR
+from historical_data import exchange_data
 import pprint
 
-exchange_id = 'okex'
+exchange_id = 'kraken'
 exchange_class = getattr(ccxt, exchange_id)
 exchange = exchange_class({
     'apiKey': 'YOUR_API_KEY',
@@ -18,11 +20,29 @@ exchange = exchange_class({
 
 # ======== Trading Qty Parameters =========
 balance = 500 # Dollar amount
-perc_trading_size = 0.25 # How much % of portfolio allocated to every trade
+per_trade_alloc = 0.05 # How much % of portfolio allocated to every trade
+
+def position_dollar_value(symbol, per_trade_alloc, timeframe):
+    '''
+    :param symbol: for ex: 'BTC/USD', 'ETH/USD' etc
+    :param per_trade_alloc: A number between 0 and 1 (Percentage of portfolio per trade)
+    :param timeframe: For ex: '1m', '30m', '1hr', '4hr' etc
+    :return:
+    '''
+    exchange = exchange_data('kraken', 'BTC/USD', timeframe=timeframe,
+                             since=datetime.datetime.now() + datetime.timedelta(-30))
+    data = pd.DataFrame(exchange,
+                        columns=['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
+    atr = ATR(data, 30)
+    N = atr['ATR'].ewm(span=30, adjust=False).mean()
+    dollar_val_per_trade = per_trade_alloc * balance / N
 
 
 # ========= Visualise market depth ==========
 def order_book(exchange, symbol, limit):
+    '''
+    :return: Bid, Ask prices from order book
+    '''
     # exchange = getattr(ccxt, exchange)()
     # exchange.load_markets()
     data = exchange.fetch_order_book(symbol,limit)
@@ -33,17 +53,20 @@ def order_book(exchange, symbol, limit):
 
 # ========= Mimicking close price as avg of bid-ask ===========
 def mid_price(exchange, symbol):
+    '''
+    :return: A pseudo alternate for Last traded price
+    '''
     bid,ask = order_book(exchange, symbol, 5)
     mid_price = (bid+ask)/2
     return mid_price
 
 
 # ========= Create Buy/Sell Orders when strategy conditions are satisfied ==========
-def create_trade(exchange, symbol):
+def create_trade(exchange, symbol, qty):
     buy_condition = False
     sell_condition = False
 
-    qty = balance * perc_trading_size/mid_price(exchange,symbol)
+    # qty = position_dollar_value(symbol, per_trade_alloc=per_trade_alloc, timeframe=timeframe)/mid_price(exchange, symbol)
     while len(exchange.private_post_positions()) == 0:
         if buy_condition:
             buy_order = exchange.create_limit_buy_order(symbol, qty,
@@ -56,12 +79,12 @@ def create_trade(exchange, symbol):
 
 
 # ========== Create strategy exit orders ================
-def closeout_trade(exchange, symbol):
+def closeout_trade(exchange, symbol, qty):
     buy_condition = False
     sell_condition = False
 
-    qty = balance * perc_trading_size / mid_price(exchange, symbol)
-    while exchange.private_post_positions() > 0:
+    # qty = position_dollar_value(symbol, per_trade_alloc=per_trade_alloc, timeframe=timeframe)/mid_price(exchange, symbol)
+    while len(exchange.private_post_positions()) > 0:
         if exchange.private_post_positions()['side'] == 'buy' and sell_condition:
             close_buy = exchange.create_limit_sell_order(symbol, qty,
                                                          order_book(exchange, symbol, 5)[0])
