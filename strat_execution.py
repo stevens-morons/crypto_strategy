f1fa6431d11feb67435d27d5055052c6207c7a1f
stevens-morons <42627085+stevens-morons@users.noreply.github.com>
2018-09-17 14:09:53 -0400

    Update strat_execution.py

--- strat_execution.py
+++ strat_execution.py
@@ -10,7 +10,7 @@ import oms
 
 # ==== Initial exchange parameters =====
 symbol = str('BTC/USD')
-symbol_list = ['BTC/USD','ETH/USD']
+symbol_list = ['BTC/USD', 'ETH/USD']
 timeframe = str('1d')
 # exchange = str('okex')
 start_date = str('2018-01-01')
@@ -26,16 +26,35 @@ exchange = exchange_class({
     'enableRateLimit': True,
 })
 
+def main():
+    while exchange.fetch_balance() > 0:
+        for sym in symbol_list:
+            init_macross(exchange, sym)
+
+            oms.trade_details(sym)
+            oms.exchange_balances()
+    time.sleep(60)
+
+# ===================== Data for various Timeframes ==========================
+data_1m = exchange_data(exchange_id, symbol, timeframe='1m', since=start_date)
+data_5m = exchange_data(exchange_id, symbol, timeframe='5m', since=start_date)
+data_15m = exchange_data(exchange_id, symbol, timeframe='15m', since=start_date)
+data_30m = exchange_data(exchange_id, symbol, timeframe='30m', since=start_date)
+data_1hr = exchange_data(exchange_id, symbol, timeframe='1hr', since=start_date)
+data_4hr = exchange_data(exchange_id, symbol, timeframe='4hr', since=start_date)
+
+
 # =============== Strategy 1 - Moving average crossover =================
-data_macross = exchange_data(exchange_id, symbol, timeframe='1m', since=start_date)
 def init_macross(exchange,symbol):
-    ema_short = strat_movavgcross.ema(data_macross, ema_period)
+    ema_short = strat_movavgcross.ema(data_1m, ema_period)
     bid, ask = oms.order_book(exchange, symbol, 5)
     buy_condition = bid > ema_short
-    sell_condition = ask < ema_shor
+    sell_condition = ask < ema_short
 
-    init_trade = oms.create_trade(exchange, symbol)
-    exit_trade = oms.closeout_trade(exchange, symbol)
+    init_qty = oms.position_dollar_value(symbol, per_trade_alloc=0.05, timeframe='1m') / oms.mid_price(exchange,symbol)
+    init_trade = oms.create_trade(exchange, symbol, qty=init_qty)
+    exit_trade = oms.closeout_trade(exchange, symbol, qty=init_qty)
+
 
 
 # =============== Strategy 2  ================================
@@ -47,3 +66,8 @@ Details of Strategy 2
 '''
 Details of Strategy 3
 '''
+
+if __name__=='__main__':
+    main()
+
+
