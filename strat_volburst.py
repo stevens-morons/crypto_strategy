c6c439aed7a997c3937e90cb0d146f422c0018a0
stevens-morons <42627085+stevens-morons@users.noreply.github.com>
2019-09-07 15:42:44 -0400

    Updated Volatility burst strategy
    Backtest code with different periods

--- strat_volburst.py
+++ strat_volburst.py
@@ -1,13 +1,15 @@
+import import_ipynb
 import pandas as pd
 import numpy as np
 from time import time
 import threading
 import matplotlib.pyplot as plt
 # import pyfolio as pf
-import csv;
-import datetime;
+import csv
+import datetime
 import pytz
-from technical_indicators import BBANDS
+# from technical_indicators import BBANDS,STDDEV
+# from technical_indicators import ATR
 from historical_data import exchange_data, write_to_csv, to_unix_time
 # import backtest
 import warnings
@@ -28,11 +30,31 @@ borrow_cost = 0.0026    # === Assuming every short trade is 100 Hours
 # kraken = exchange_data('kraken', 'BTC/USD', timeframe=timeframe, since=hist_start_date)
 # write_to_csv(kraken, 'BTC/USD', 'kraken')
 # data = pd.DataFrame(kraken, columns=header)
-data = pd.read_csv("gemini_BTCUSD_1hr.csv")
+data = pd.read_csv("kraken-BTCUSD-1h.csv")
+
+#Standard Deviation
+def STDDEV(df, n):
+    STD=pd.Series(df['Close'].rolling(n).std(),name='Volatility')
+    df=df.join(STD)
+    return df
+
+#Bollinger Bands
+def BBANDS(df, stddev, n):
+    MA_high = pd.Series(df['High'].rolling(n).mean())
+    MA_low = pd.Series(df['Low'].rolling(n).mean())
+    MSD = pd.Series(df['Close'].rolling(n).std())
+    b1 = MA_high + stddev * MSD
+    B1 = pd.Series(b1, name = 'BB_Upper')
+    df = df.join(B1)
+    b2 = MA_low - stddev * MSD
+    B2 = pd.Series(b2, name = 'BB_Lower')
+    df = df.join(B2)
+    return df
 # =================================================================
 # ================ DIRECTIONAL STRATEGY ===========================
-avg_true_range = ATR(data,24)
-slippage = 0.0001 * avg_true_range['ATR'] # === Slippage as a function of Volatility
+#avg_true_range = ATR(data,24)
+volatility = STDDEV(data,24)
+slippage = 0.0001 * volatility['Volatility'] # === Slippage as a function of Volatility
 stddev = 2
 
 def strategy(data, list_periods):
@@ -77,14 +99,14 @@ def strategy(data, list_periods):
         # data['strat_returns'] = data['position'].shift(1) * data['returns']
         # print str(period) + "Period : " + data['strat_returns']
         cum_returns = data['strat_returns'].dropna().cumsum()
-        print '\nCumulative Percentage Returns for ' + str(period) + ' period Volatility Burst: {}%'.format(round(cum_returns[-1:], 2))
-
+        print('\nCumulative Percentage Returns for ' + str(period) + ' period Volatility Burst: {}%'.format(round(cum_returns[-1:], 2)))
+              
     return data['strat_returns']
 
 list_periods = range(10, 50, 5)
 returns = strategy(data=data, list_periods=list_periods)
 end = time()
-print '\nTotal time for execution: {} secs '.format(round(end - start), 2)
+print('\nTotal time for execution: {} secs '.format(round(end - start), 2))
 
 # backtest.drawdown_periods(returns)
 # backtest.underwater_plot(returns)
